e0f4e417b7d43c0341172ebf09024e2620b4270c
Naim Abdelkerim <naimdevelopper@gmail.com>
2023-03-15 19:03:44 +0100

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+  redirect,
+  
+} from "react-router-dom";
+import RestaurantHome from './pages/RestaurantPage';
+import Home from './pages/Home';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index loader={()=>redirect("/abcd1234efgh5678")} element={<Home/>} />
+      <Route path=":clientId" element={<RestaurantHome />} >
+        <Route index element={<div>resto home</div>} />
+
+      </Route>
+    </Route>
+  )
+);
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div className=''>{count}</div>
+    <main>
+      <RouterProvider router={router} />
+    </main>
   )
 }
 
 export default App
+
---
+++ src/pages/RestaurantPage/index.tsx
@@ -0,0 +1,5 @@
+function RestaurantHome() {
+    return ( <div>resto hfdfome</div> );
+}
+
+export default  RestaurantHome;
